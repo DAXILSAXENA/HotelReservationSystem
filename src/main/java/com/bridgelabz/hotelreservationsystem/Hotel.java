dcc5c3d102ae33a04c134b8697dc4ba0dcfd48f5
DAXILSAXENA <daxildaxil@gmail.com>
2021-07-26 20:18:03 +0530

    [Daxil] Added method to Add Hotel Details

---
+++ src/main/java/com/bridgelabz/hotelreservationsystem/Hotel.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class Hotel {
+	private String hotelName;
+	private int regularCustomerRate;
+
+	public Hotel(String hotelName, int regularCustomerRate) {
+		this.hotelName = hotelName;
+		this.regularCustomerRate = regularCustomerRate;
+	}
+
+}
